0214fecac50102af3c101e16824908f2213536b3
zer <zach.earos@gmail.com>
2014-10-15 17:26:23 +0100

    made MC iterate, adding testing functions of units

--- monte_carlo.py
+++ monte_carlo.py
@@ -2,31 +2,32 @@ import diffusion_model as dm
 from random import random
 from math import exp
 
-def monte_carlo(energy,density,temperature):
-    """inputs of some energy function, a given density and a temperature"""
-    #keep value of input density
-    first_density = density[:]
-    #compute the energy of the system.
-    energy_val = energy(density)
-    #perturb the density by moving a particle
-    second_density = perturb(density)
-    E0 = float(energy(first_density))
-    E1 = float(energy(second_density))
-    temperature = float(temperature)
-    
-    #if new energy is less, keep that.
-    if E0 > E1:
-        density = second_density
-    #or if the boltzmann exceeds a random number in [0,1]
-    elif boltzmann(E0,E1,temperature) > random():
-        print 'boing'
-        density = second_density
-    else:
-        density = first_density
+def monte_carlo(energy,density,temperature,iterations):
+    """inputs of some energy function, a given density, a temperature and a number of iterations """
+    for ii in range(iterations):
+        #keep value of input density
+        first_density = density[:]
+        #compute the energy of the system.
+        energy_val = energy(density)
+        #perturb the density by moving a particle
+        box_no = which_box_loses_a_particle(density)
+        second_density = move_particle_one(density,box_no)
+        E0 = float(energy(first_density))
+        E1 = float(energy(second_density))
+        temperature = float(temperature)
+        
+        #if new energy is less, keep that.
+        if E0 > E1:
+            density = second_density
+        #or if the boltzmann exceeds a random number in [0,1]
+        elif boltzmann(E0,E1,temperature) > random():
+            density = second_density
+        else:
+            density = first_density
     return density
 
-def perturb(density):
-    #will pick a random particle in the density and move it either left or right
+def which_box_loses_a_particle(density):
+    #will pick a random particle in the density and return the box number
     num_particles = sum(density)
     particle_we_move =  random() * num_particles
     #here, we see which box the particle lived in - when we tick past the counter, then it was that box
@@ -36,7 +37,7 @@ def perturb(density):
         ctr += density[i]
         box_no = i
         i += 1
-    return move_particle_one(density,box_no)
+    return box_no
 
 #moves particle in box box_no one to left or right, at random.
 #circular boundary conditions
@@ -51,8 +52,3 @@ def move_particle_one(density,box_no):
 #calculates the boltzmann distribution
 def boltzmann(E0,E1,T):
     return exp(-(E1-E0)/T)
-
-density = [1] * 50
-for i in range(100):
-    density = monte_carlo(dm.energy,density ,1000)
-    print density
--- test_diffusion_model.py
+++ test_diffusion_model.py
@@ -1,4 +1,6 @@
 from diffusion_model import energy
+import monte_carlo as MC
+
 def test_energy():
     density = range(1,6)
     assert energy(density) == 20
@@ -10,3 +12,9 @@ def test_energy_empty():
 def test_large_array_of_zeros():
     density = [0] * 1000000 
     assert energy(density) == 0
+
+#tests for move_particle_one
+#check that it hasn't changed the number
+def test_same_number_at_end():
+    density = [1,0,2] * 100
+    assert sum(density) == sum(MC.move_particle_one(density,3))
